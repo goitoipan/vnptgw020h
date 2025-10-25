4b745f3aa51adf67d8b6e14d0073c95f02a08a7b
NotFkrystal <108082159+BussyBakks@users.noreply.github.com>
2025-10-25 20:36:52 +0700

    v5

--- Integrations/ddns_updater/install.sh
+++ Integrations/ddns_updater/install.sh
@@ -2,7 +2,7 @@
 cd /tmp/userdata
 rm -rf ddns_updater
 mkdir ddns_updater
-/userfs/bin/curl -Lk -o ddns_updater/updater_v5 https://github.com/qdm12/ddns-updater/releases/download/v2.9.0/ddns-updater_2.9.0_linux_armv7
+/userfs/bin/curl -Lk -o ddns_updater/updater_v5 https://github.com/qdm12/ddns-updater/releases/download/v2.9.0/ddns-updater_2.9.0_linux_armv5
 cd ddns_updater
 chmod 777 updater_v5
 ./updater_v5
