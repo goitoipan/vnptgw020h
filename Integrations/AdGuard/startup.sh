bcc826be394e7b7cdd50c395c5e14338c0f49d77
AppleSang <96937073+AppleSang@users.noreply.github.com>
2026-01-12 22:35:47 +0700

    Update startup.sh

--- Integrations/AdGuard/startup.sh
+++ Integrations/AdGuard/startup.sh
@@ -5,14 +5,14 @@
 #
 # Note: Startup script for AdGuardHome
 
-if [ ! -e /tmp/SafeGate/AdGuardHome ]; then
+if [ ! -e /tmp/AdGuardHome ]; then
     export SSL_CERT_FILE=/tmp/userdata/AdGuard/ca.crt
-    cd /tmp/SafeGate || exit 1
+    cd /tmp/ || exit 1
     /userfs/bin/curl -s -fSL -o AdG_armv5l.tar.gz https://github.com/AdguardTeam/AdGuardHome/releases/latest/download/AdGuardHome_linux_armv5.tar.gz
     tar -xzf AdG_armv5l.tar.gz
     rm AdG_armv5l.tar.gz
     cd AdGuardHome || exit 1
     chmod +x AdGuardHome
     pidof dnsmasq >/dev/null && kill $(pidof dnsmasq)
-    ./AdGuardHome -c /tmp/userdata/AdGuard/AdGuardHome.yaml -w /tmp/SafeGate
+    ./AdGuardHome -c /tmp/userdata/AdGuard/AdGuardHome.yaml -w /tmp/
 fi
